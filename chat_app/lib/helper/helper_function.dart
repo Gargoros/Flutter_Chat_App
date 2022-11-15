ed8fcdfc722a4e5d4857ff7cddfbadf66f23d71e
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-11-16 01:44:23 +0300

    feat: init new functions to save user data

--- chat_app/lib/helper/helper_function.dart
+++ chat_app/lib/helper/helper_function.dart
@@ -7,6 +7,20 @@ class HelperFunction {
   static String userEmaiKey = "USEREMAILKEY";
 
   //save data to sf
+  static Future<bool> saveUserLoggedInStatus(bool isUserLoggedIn) async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return await sf.setBool(userLoginKey, isUserLoggedIn);
+  }
+
+  static Future<bool> saveUserNameSF(String userName) async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return await sf.setString(userNameKey, userName);
+  }
+
+  static Future<bool> saveUserEmailSF(String userEmail) async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return await sf.setString(userEmaiKey, userEmail);
+  }
 
   //get data from sf
   static Future<bool?> getUserLoggedInStatus() async {
