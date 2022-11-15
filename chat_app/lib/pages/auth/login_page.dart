4f12266bd4dcd4e7469c0c836a16706075cceadf
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-11-15 22:10:58 +0300

    faet: init login page

---
+++ chat_app/lib/pages/auth/login_page.dart
@@ -0,0 +1,66 @@
+import 'package:chat_app/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 80),
+          child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    "Let's Talk",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "Login now and join to our talks.",
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                  Image.asset(
+                    "assets/images/login_logo.jpg",
+                  ),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(
+                      label: const Text("Email"),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(
+                        label: const Text("Password"),
+                        prefixIcon: Icon(
+                          Icons.key,
+                          color: Theme.of(context).primaryColor,
+                        )),
+                  ),
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
