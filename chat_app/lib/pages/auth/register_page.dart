92b7f8b554e238b4d5cbfdde05ab5598782d7660
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-11-16 00:11:37 +0300

    feat: init new text field to registeration

--- chat_app/lib/pages/auth/register_page.dart
+++ chat_app/lib/pages/auth/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/pages/auth/login_page.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,7 @@ class _RegisterPageState extends State<RegisterPage> {
   final formKey = GlobalKey<FormState>();
   String userEmail = "";
   String userPassword = "";
-  String fullName = "";
+  String userFullName = "";
 
   @override
   Widget build(BuildContext context) {
@@ -36,11 +37,36 @@ class _RegisterPageState extends State<RegisterPage> {
                     height: 10,
                   ),
                   const Text(
-                    "Login now and join to our talks.",
+                    "Create your account now to chat and explore.",
                     style: TextStyle(color: Colors.black54),
                   ),
                   Image.asset(
-                    "assets/images/login_logo.jpg",
+                    "assets/images/register_logo.jpeg",
+                  ),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(
+                      label: const Text("Full Name"),
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    textInputAction: TextInputAction.next,
+                    onChanged: ((value) {
+                      setState(() {
+                        userFullName = value;
+                      });
+                    }),
+                    validator: (value) {
+                      if (value!.isNotEmpty) {
+                        return null;
+                      } else {
+                        return "Name can not be empty!";
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
                   ),
                   TextFormField(
                     decoration: textInputDecoration.copyWith(
@@ -103,7 +129,7 @@ class _RegisterPageState extends State<RegisterPage> {
                             backgroundColor: Theme.of(context).primaryColor),
                         onPressed: () {},
                         child: const Text(
-                          "Sing In",
+                          "Register",
                           style: TextStyle(color: Colors.white, fontSize: 16),
                         )),
                   ),
@@ -111,18 +137,18 @@ class _RegisterPageState extends State<RegisterPage> {
                     height: 10,
                   ),
                   Text.rich(TextSpan(
-                      text: "Don't have an account? ",
+                      text: "Already have an account? ",
                       style:
                           const TextStyle(color: Colors.black54, fontSize: 14),
                       children: <TextSpan>[
                         TextSpan(
-                          text: "Register here",
+                          text: "Login now",
                           style: const TextStyle(
                               color: Colors.blue,
                               decoration: TextDecoration.underline),
                           recognizer: TapGestureRecognizer()
                             ..onTap = () {
-                              nextScreen(context, const RegisterPage());
+                              nextScreen(context, const LoginPage());
                             },
                         )
                       ]))
