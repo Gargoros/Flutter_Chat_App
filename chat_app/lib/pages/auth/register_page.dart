7be721aa21850f6a640b12b4747855b71b1471b6
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-11-15 23:32:38 +0300

    feat: init new page

--- chat_app/lib/pages/auth/register_page.dart
+++ chat_app/lib/pages/auth/register_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
+import '../../widgets/widgets.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -8,10 +11,125 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final formKey = GlobalKey<FormState>();
+  String userEmail = "";
+  String userPassword = "";
+  String fullName = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Register Page")),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 120, horizontal: 20),
+          child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    "Let's Talk",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "Login now and join to our talks.",
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                  Image.asset(
+                    "assets/images/login_logo.jpg",
+                  ),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(
+                      label: const Text("Email"),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                    onChanged: ((value) {
+                      setState(() {
+                        userEmail = value;
+                      });
+                    }),
+                    validator: (value) {
+                      return RegExp(
+                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value!)
+                          ? null
+                          : "Please enter a valid email";
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: textInputDecoration.copyWith(
+                        label: const Text("Password"),
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Theme.of(context).primaryColor,
+                        )),
+                    validator: (value) {
+                      if (value!.length < 6) {
+                        return "Password must be at least 6 characters";
+                      } else {
+                        return null;
+                      }
+                    },
+                    textInputAction: TextInputAction.next,
+                    onChanged: ((value) {
+                      setState(() {
+                        userPassword = value;
+                      });
+                    }),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30)),
+                            backgroundColor: Theme.of(context).primaryColor),
+                        onPressed: () {},
+                        child: const Text(
+                          "Sing In",
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text.rich(TextSpan(
+                      text: "Don't have an account? ",
+                      style:
+                          const TextStyle(color: Colors.black54, fontSize: 14),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "Register here",
+                          style: const TextStyle(
+                              color: Colors.blue,
+                              decoration: TextDecoration.underline),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              nextScreen(context, const RegisterPage());
+                            },
+                        )
+                      ]))
+                ],
+              )),
+        ),
+      ),
     );
   }
 }
