f95bbf28e8211045e7d758c26ca1df91138af10a
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-11-16 01:43:39 +0300

    refactor: init setState to status method

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -36,7 +36,9 @@ class _MyAppState extends State<MyApp> {
   void getUserLoggedInStatus() async {
     await HelperFunction.getUserLoggedInStatus().then((value) {
       if (value != null) {
-        _isSignedIn = value;
+        setState(() {
+          _isSignedIn = value;
+        });
       }
     });
   }
