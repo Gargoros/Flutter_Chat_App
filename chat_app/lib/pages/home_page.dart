4fc4b95e040bf8885de794186adb5d27019aef53
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-11-17 02:05:54 +0300

    feat: init appBar and drawer

--- chat_app/lib/pages/home_page.dart
+++ chat_app/lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:chat_app/pages/auth/login_page.dart';
+import 'package:chat_app/pages/search_page.dart';
 import 'package:chat_app/service/auth_service.dart';
+import 'package:chat_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,12 +16,44 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Let's Talk",
+          style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+        ),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                nextScreen(context, const SearchPage());
+              },
+              icon: const Icon(Icons.search))
+        ],
+        elevation: 0,
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
       body: Center(
           child: ElevatedButton(
-              onPressed: () {
-                authService.signOut();
-              },
-              child: const Text("LOGOUT"))),
+        onPressed: () {
+          nextScreen(context, const LoginPage());
+        },
+        child: const Text("LogOut"),
+      )),
+      drawer: Drawer(
+        child: ListView(
+            padding: const EdgeInsets.symmetric(vertical: 50),
+            children: <Widget>[
+              Icon(
+                Icons.account_circle,
+                size: 150,
+                color: Colors.grey[700],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(""),
+            ]),
+      ),
     );
   }
 }
